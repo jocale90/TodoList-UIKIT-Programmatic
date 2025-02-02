283b65b9a1f392317c8e1c83c5e7f67e8f8f8616
Jose Pernia <joseapc90@gmail.com>
2025-02-02 12:33:27 -0300

    v1.0.0 a simple table with static info from array todos

---
+++ TodoList-UIKIT-Programmatic/Todo.swift
@@ -0,0 +1,13 @@
+//
+//  Todo.swift
+//  TodoList-UIKIT-Programmatic
+//
+//  Created by Jose Pernia on 2025-02-01.
+//
+
+import Foundation
+
+struct Todo {
+    let title: String
+    var isComplete: Bool = false
+}
---
+++ TodoList-UIKIT-Programmatic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodoList-UIKIT-Programmatic
+//
+//  Created by Jose Pernia on 2025-02-01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
